6f2ac50e915f288e1a8d420e54ede4b52e2189d0
gustavodiniz <gustavo.diniz@tangerino.com.br>
2022-06-02 12:37:01 -0300

    errors handling

---
+++ clients-api/src/main/java/br/com/gustavodiniz/clientsapi/controllers/ApplicationControllerAdvice.java
@@ -0,0 +1,37 @@
+package br.com.gustavodiniz.clientsapi.controllers;
+
+import br.com.gustavodiniz.clientsapi.controllers.exceptions.ApiErrors;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+public class ApplicationControllerAdvice {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiErrors handleValidationErrors(MethodArgumentNotValidException exception) {
+        BindingResult bindingResult = exception.getBindingResult();
+        List<String> messages = bindingResult.getAllErrors()
+                .stream()
+                .map(objectError -> objectError.getDefaultMessage())
+                .collect(Collectors.toList());
+
+        return new ApiErrors(messages);
+    }
+
+    public ResponseEntity handleResponseStatusException(ResponseStatusException exception) {
+        String errorMessage = exception.getReason();
+        HttpStatus statusCode = exception.getStatus();
+        ApiErrors apiErrors = new ApiErrors(errorMessage);
+        return new ResponseEntity(apiErrors, statusCode);
+    }
+}
