040bf3bd8a72256d1edff388ff5458401c818539
gustavodiniz <gustavo.diniz@tangerino.com.br>
2022-06-02 13:07:21 -0300

    added security configurations

--- clients-api/src/main/java/br/com/gustavodiniz/clientsapi/repositories/UserRepository.java
+++ clients-api/src/main/java/br/com/gustavodiniz/clientsapi/repositories/UserRepository.java
@@ -3,6 +3,10 @@ package br.com.gustavodiniz.clientsapi.repositories;
 import br.com.gustavodiniz.clientsapi.models.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<UserModel, Integer> {
     boolean existsByUsername(String username);
+
+    Optional<UserModel> findByUsername(String username);
 }
--- clients-api/src/main/java/br/com/gustavodiniz/clientsapi/services/impl/UserServiceImpl.java
+++ clients-api/src/main/java/br/com/gustavodiniz/clientsapi/services/impl/UserServiceImpl.java
@@ -7,10 +7,14 @@ import br.com.gustavodiniz.clientsapi.repositories.UserRepository;
 import br.com.gustavodiniz.clientsapi.services.UserService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserServiceImpl implements UserService {
+public class UserServiceImpl implements UserService, UserDetailsService {
 
     @Autowired
     private UserRepository userRepository;
@@ -25,5 +29,16 @@ public class UserServiceImpl implements UserService {
             throw new UserAlreadyRegisteredException(userDTO.getUsername());
         }
         return userRepository.save(modelMapper.map(userDTO, UserModel.class));
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserModel userModel = userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("Username not found"));
+        return User
+                .builder()
+                .username(userModel.getUsername())
+                .password(userModel.getPassword())
+                .roles("USER")
+                .build();
     }
 }
